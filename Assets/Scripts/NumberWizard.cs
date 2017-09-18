00a7c7f51552509bfcc5c6201003aea67263c3bc
Faelwen <francis.john.mccarthy@googlemail.com>
2017-09-19 00:37:10 +0200

    Using if

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -24,10 +24,11 @@ public class NumberWizard : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			print ("Up arrow pressed");
-		}
-		if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			print ("Down arrow pressed");
+			print ("Up arrow pressed.");
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			print ("Down arrow pressed.");
+		} else if (Input.GetKeyDown (KeyCode.Return)) {
+			print ("I won!");
 		}
 	}
 }
