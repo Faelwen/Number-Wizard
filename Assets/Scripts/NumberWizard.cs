04452812e4315870a06d6545d8d556e7a20238be
Faelwen <francis.john.mccarthy@googlemail.com>
2017-09-19 00:42:35 +0200

    Scope and Context of Variables

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
+	int min = 0;
+	int max = 1000;
+	int guess = 500;
+
 	void Start () {
-		int min = 0;
-		int max = 1000;
+		max++;
 
 		print ("Welcome to Number Wizard");
 		print ("Pick a number in your head but don't tell me.");
@@ -15,7 +18,7 @@ public class NumberWizard : MonoBehaviour {
 		print ("The lowest number you can pick is " + min);
 		print ("The highest number you can pick is " + max);
 
-		print ("Is the number higher or lower than 500?");
+		print ("Is the number higher or lower than " + guess + "?");
 		print ("Up arrow for higher, down for lower, return for equal.");
 
 
@@ -24,9 +27,13 @@ public class NumberWizard : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			print ("Up arrow pressed.");
+			min = guess;
+			guess = (max + min) / 2;
+			print ("Higher or lower than " + guess + "?");
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			print ("Down arrow pressed.");
+			max = guess;
+			guess = (max + min) / 2;
+			print ("Higher or lower than " + guess + "?");
 		} else if (Input.GetKeyDown (KeyCode.Return)) {
 			print ("I won!");
 		}
