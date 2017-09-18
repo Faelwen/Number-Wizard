fcc0166ee40d809b0ce5044265e01f4ce6200d63
Faelwen <francis.john.mccarthy@googlemail.com>
2017-09-19 00:30:23 +0200

    Responding To Key Presses

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -11,12 +11,23 @@ public class NumberWizard : MonoBehaviour {
 
 		print ("Welcome to Number Wizard");
 		print ("Pick a number in your head but don't tell me.");
+
 		print ("The lowest number you can pick is " + min);
 		print ("The highest number you can pick is " + max);
+
+		print ("Is the number higher or lower than 500?");
+		print ("Up arrow for higher, down for lower, return for equal.");
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			print ("Up arrow pressed");
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			print ("Down arrow pressed");
+		}
 	}
 }
