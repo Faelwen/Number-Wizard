1f33ad3e640638e432cf85d25339fb14590f8c36
Faelwen <francis.john.mccarthy@googlemail.com>
2017-09-19 00:18:28 +0200

    Using Variables

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -6,8 +6,13 @@ public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		int min = 0;
+		int max = 1000;
+
 		print ("Welcome to Number Wizard");
 		print ("Pick a number in your head but don't tell me.");
+		print ("The lowest number you can pick is " + min);
+		print ("The highest number you can pick is " + max);
 	}
 	
 	// Update is called once per frame
